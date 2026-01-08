bfbe02a4ee85bdd79cddb819463bf50faa62d725
PMagoga <pmagoga@gmail.com>
2026-01-08 14:20:30 -0300

    add icones

--- electron_react/electron.vite.config.ts
+++ electron_react/electron.vite.config.ts
@@ -1,16 +1,29 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  main: {},
+  main: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "resources/*",
+            dest: "resources",
+          },
+        ],
+      }),
+    ],
+  },
   preload: {},
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        "@renderer": resolve("src/renderer/src"),
+      },
     },
-    plugins: [react()]
-  }
-})
+    plugins: [react(), tailwindcss()],
+  },
+});
